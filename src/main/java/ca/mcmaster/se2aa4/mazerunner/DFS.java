a24f4c73e9f807bd338a418fbbc17c4093842e7b
Andrew Bovbel <andrew@bovbel.com>
2024-04-08 11:20:27 -0400

    feat: business logic is complete

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/DFS.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/DFS.java
@@ -9,119 +9,69 @@ import java.util.List;
 import java.util.Set;
 
 public class DFS implements MazeSolver{
-
     private Tile e;
-    private Maze.Path toReturn;
-
     private ArrayList<DirectionTilePair> path = new ArrayList<>();
-
     private final Set<Tile> marked = new HashSet<>();
     public Maze.Path solve(Maze maze)  {
         Position start = maze.getStart();
         Position end = maze.getEnd();
-
-//        System.out.println(start);
-//        System.out.println(end);
-
-
         Tile t = maze.get(start.y(), start.x()).get();
         e = maze.get(end.y(), end.x()).get();
-        Direction facing = Direction.RIGHT;
-
-        newdfs(new DirectionTilePair(Direction.RIGHT, t));
-        path.removeFirst();
-        System.out.println(path);
-        return getpath(path);
-
-//        try {
-//            return dfs(t, facing, new Maze.Path());
-//        } catch (CloneNotSupportedException ex) {
-//            throw new RuntimeException(ex);
-//        }
-    }
-
-    private Maze.Path getpath(ArrayList<DirectionTilePair> path) {
-        Maze.Path lol = new Maze.Path();
-        Direction facing = Direction.RIGHT;
-        for (DirectionTilePair to : path) {
-            facing = delta(facing, to.direction(),lol);
+        DirectionTilePair first = new DirectionTilePair(Direction.RIGHT, t);
+        try {
+            dfs(first);
+        } catch (TerminateRecursionException e) {
+            path = e.getCurrentState();
         }
-
-        return lol;
+        path.removeFirst();
+        return getpath(path);
     }
 
-    private void newdfs(DirectionTilePair t) {
+    private Maze.Path getpath(ArrayList<DirectionTilePair> nodeCollection) {
+        Maze.Path path = new Maze.Path();
+        Direction facing = Direction.RIGHT;
+        for (DirectionTilePair to : nodeCollection) {
+            facing = delta(facing, to.direction(), path);
+        }
+        return path;
+    }
+
+    private void dfs(DirectionTilePair t) {
 
         marked.add(t.tile());
         path.add(t);
 
         if (t.tile().equals(e)) {
-            return;
+            //if end node is found, terminate recursion and return path
+            throw new TerminateRecursionException(path);
         }
 
-        Boolean lol = false;
+        //backtracking algorithm to remove unnecessary dead ends
+        //first check if current node has unchecked neighbours
+        boolean hasNeighboursUnmarked = false;
         for (DirectionTilePair w : t.tile().adj()) {
             if (!marked.contains(w.tile())) {
-                lol = true;
+                hasNeighboursUnmarked = true;
+                break;
             }
         }
-        if (!lol) {
+        //if all neighbours have been visited, i.e. is a dead end, backtrack until we hit the turning point
+        if (!hasNeighboursUnmarked) {
             DirectionTilePair last = path.getLast();
-//            System.out.println(last.tile().adj().stream());
             while (last.tile().adj().stream().allMatch(x -> marked.contains(x.tile()))) {
                 path.removeLast();
-
-                System.out.println(last.direction());
                 last = path.getLast();
-//                System.out.println("hjo");
-//                System.out.println(last.direction());
-//                last = path.removeLast();
             }
-
             return;
         }
 
+        //continue dfs
         for (DirectionTilePair w : t.tile().adj()) {
             if (!marked.contains(w.tile())) {
-                newdfs(w);
-//                return;
-                           }
-        }
-
-
-
-    }
-
-
-    // depth first search from t
-        private Maze.Path dfs(Tile t, Direction facing, Maze.Path path) throws CloneNotSupportedException {
-
-            marked.add(t);
-            System.out.println(t.getType());
-
-            for (DirectionTilePair w : t.adj()) {
-                if (w.tile().equals(e)) {
-                    Direction to = w.direction();
-                    System.out.println(to);
-                    delta(facing, to, path);
-                    return path;
-                }
-                if (!marked.contains(w.tile())) {
-                    Direction to = w.direction();
-                    System.out.println(to);
-                    facing = delta(facing, to, path);
-                    Maze.Path result = dfs(w.tile(), facing, path);
-                    if (result != null) {
-                        Maze.Path cloned = result.clone();
-                        System.out.println(cloned);
-                        return result; // Return the result if found
-                    }
-                }
+                dfs(w);
             }
-
-            return null;
-
         }
+    }
 
     private Direction delta (Direction facing, Direction to, Maze.Path path) {
         switch(facing) {
@@ -253,7 +203,15 @@ public class DFS implements MazeSolver{
         return facing;
     }
 
+    static class TerminateRecursionException extends RuntimeException {
+        private final ArrayList<DirectionTilePair> currentState;
 
+        public TerminateRecursionException(ArrayList<DirectionTilePair> currentState) {
+            this.currentState = currentState;
+        }
 
-
+        public ArrayList<DirectionTilePair> getCurrentState() {
+            return currentState;
+        }
+    }
 }
